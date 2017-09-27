4824e9b2a455017871aa004c1b0ee1f713eea02e
ahmed-osama-iv <31954235+ahmed-osama-iv@users.noreply.github.com>
2017-09-27 21:36:25 +0200

    Create Fast_Power.cpp

---
+++ Fast_Power.cpp
@@ -0,0 +1,12 @@
+ll __pow(ll base, ll power, ll mod=MOD){
+    ll result = 1;
+    while(power > 0) {
+
+        if(power & 1){ // power % 2 == 1
+            result = (result*base) % mod;
+        }
+        base = (base * base) % mod;
+        power >>= 1; // power = power / 2;
+    }
+    return result;
+}
