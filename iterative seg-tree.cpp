4dde7927e618a0cc89bcc753bb68d35c031b670c
ahmed-osama-iv <31954235+ahmed-osama-iv@users.noreply.github.com>
2018-08-20 06:28:15 +0200

    Update iterative seg-tree.cpp

--- iterative seg-tree.cpp
+++ iterative seg-tree.cpp
@@ -1,62 +1,76 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define MAX 200005
-#define inf 1000000009
- 
-using namespace std; 
- 
-struct seg_tree{
- 
-    const static int n=1<<20; //size of array
-    int tree[2*n];
-    int e=0; //identity
- 
+#include<bits/stdc++.h>
+using namespace std;
+
+struct SegTree{
+
+    int n, e, *tree;
     int (*op)(int,int); // function of get
- 
-    int get(int a, int b) {
-        a += n; b += n;
-        int s = e;
-        while (a <= b) {
-            if (a%2 == 1) s = op(s, tree[a++]);
-            if (b%2 == 0) s = op(s, tree[b--]);
-            a /= 2; b /= 2;
-        }
-        return s;
+
+    SegTree(){}
+
+    SegTree(int size, int (*operation)(int,int), int identity){
+        n = 1 << size;
+        op = operation;
+        e = identity;
+        tree = new int[2*n];
+        clear();
     }
- 
- 
-    void update(int k, int x){ //update index k by value x
-        k += n;
-        tree[k] = x;
-        for (k /= 2; k >= 1; k /= 2) {
-            tree[k] = op(tree[2*k], tree[2*k+1]);
+
+    void clear(){
+        for(int i=0; i<2*n; i++) tree[i] = e;
+    }
+
+    int get(int idx){
+        return tree[idx + n];
+    }
+
+    void set(int idx, int val){
+        tree[idx + n] = val;
+    }
+
+    int get(int l, int r){
+        l += n; r += n;
+        int res = e;
+        while(l <= r){
+            if (l%2 == 1) res = op(res, tree[l++]);
+            if (r%2 == 0) res = op(res, tree[r--]);
+            l /= 2; r /= 2;
+        }
+        return res;
+    }
+
+    void update(int idx, int val){ //update index by value
+        idx += n;
+        tree[idx] = val;
+        for(idx /= 2; idx >= 1; idx /= 2){
+            tree[idx] = op(tree[2*idx], tree[2*idx + 1]);
         }
     }
- 
+
+    void build(){
+        for(int i=n-1; i>0; i--){
+            tree[i] = op(tree[2*i], tree[2*i + 1]);
+        }
+    }
+
 };
- 
-int sum(int a, int b){return a+b;}
-int maxx(int a, int b){return max(a,b);}
- 
-seg_tree seg1, seg2;
- 
+
+int sum (int a ,int b){return a + b;}
+int minn(int a, int b){return min(a, b);}
+
 int main(){
- 
- 
-    //segment tree of sum
-    seg1.e=0, seg1.op=&sum;
-    seg1.update(1, 1);
-    seg1.update(2, 2);
-    seg1.update(3, 3);
-    cout << seg1.get(2, 8) << endl;
- 
- 
- 
-    //segment tree of max
-    seg2.e=-inf, seg2.op=&maxx;
-    seg2.update(1, 123);
-    seg2.update(2, 23);
-    seg2.update(3, 60);
-    cout << seg2.get(2, 3) << endl;
- 
+
+    SegTree segOfSum(20, sum, 0), segOfMin(20, minn, 999999);
+    segOfSum.update(2, 5);
+    segOfSum.update(4, 10);
+    segOfSum.update(10, 20);
+    cout << segOfSum.get(1, 11) << " " << segOfSum.get(2, 40) << " " << segOfSum.get(10) << endl;
+
+    segOfMin.set(0, 5);
+    segOfMin.set(1, 9);
+    segOfMin.set(2, 1);
+    segOfMin.set(3, 4);
+    segOfMin.set(4, 8);
+    segOfMin.build();
+    cout << segOfMin.get(1, 4) << " " << segOfMin.get(0, 1) << " " << segOfMin.get(3, 100) << endl;
 }
