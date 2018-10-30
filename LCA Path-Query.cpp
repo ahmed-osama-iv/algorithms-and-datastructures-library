ab0be36d8ce491ef6ffa311d32da62105058acd9
ahmed-osama-iv <31954235+ahmed-osama-iv@users.noreply.github.com>
2018-10-30 17:51:34 +0200

    Update LCA Path-Query.cpp

--- LCA Path-Query.cpp
+++ LCA Path-Query.cpp
@@ -7,20 +7,20 @@ int ans[maxV][maxLog];
 void buildLCA(int v, int p) { // O(NlogN)
 
 	anc[v][0] = p;
-    lvl[v] = lvl[p] + 1;
+	lvl[v] = lvl[p] + 1;
 
-    for(int i = 0; i < maxLog - 1; i++) {
-        anc[v][i+1] = anc[anc[v][i]][i];
-        ans[v][i+1] = max(ans[v][i], ans[anc[v][i]][i]);
-    }
+	for(int i = 0; i < maxLog - 1; i++) {
+		anc[v][i+1] = anc[anc[v][i]][i];
+		ans[v][i+1] = max(ans[v][i], ans[anc[v][i]][i]);
+	}
 
 	for(auto e : adj[v]) {
-        int u = e.F;
-        int cost = e.S;
-        if(u != p) {
-            ans[u][0] = cost;
-            buildLCA(u, v);
-        }
+		int u = e.F;
+		int cost = e.S;
+		if(u != p) {
+			ans[u][0] = cost;
+			buildLCA(u, v);
+		}
 	}
 }
 
@@ -47,7 +47,7 @@ int LCA(int u, int v) { // O(LogN)
 
 int pathAns(int u, int v) {
 
-    int res = 0;
+	int res = 0;
 
 	if(lvl[u] < lvl[v]) swap(u, v);
 
@@ -67,10 +67,9 @@ int pathAns(int u, int v) {
 			u = anc[u][i], v = anc[v][i];
 		}
 	}
-	
+
 	res = max(res, ans[u][0]);
-    res = max(res, ans[v][0]);
+	res = max(res, ans[v][0]);
 
 	return res;
 }
-
