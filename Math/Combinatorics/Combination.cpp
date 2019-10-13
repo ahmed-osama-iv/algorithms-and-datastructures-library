6c527526d069844770bf5523adaf3ca7c64335d6
ahmed-osama-iv <31954235+ahmed-osama-iv@users.noreply.github.com>
2019-10-13 16:21:55 +0200

    Create Combination.cpp

---
+++ Math/Combinatorics/Combination.cpp
@@ -0,0 +1,15 @@
+
+ll C[MAX][MAX];
+
+void build_nCr(){  // O(n^2)
+
+    C[0][0] = 1;
+    
+    for(int i=1; i<MAX; i++){
+        C[i][0] = 1;
+        for(int j=1; j<=i; j++){
+            C[i][j] = C[i-1][j] + C[i-1][j-1];
+            C[i][j] %= MOD;
+        }
+    }
+}
