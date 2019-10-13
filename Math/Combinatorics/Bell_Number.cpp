58a83efcc8f527319786ff8fab7bffed60d2691c
ahmed-osama-iv <31954235+ahmed-osama-iv@users.noreply.github.com>
2019-10-13 16:24:31 +0200

    Create Bell_Number.cpp

---
+++ Math/Combinatorics/Bell_Number.cpp
@@ -0,0 +1,16 @@
+
+ll Bell[MAX];
+
+void build_bell(){  // O(n^2)
+
+    ll sum = 0;
+    Bell[0] = 1;
+    for(int i = 1; i < MAX; i++){
+        sum = 0;
+        for(int j = 0; j < i; j++){
+            sum += Bell[j] * C[i-1][j];
+            sum %= MOD;
+        }
+        Bell[i] = sum;
+    }
+}
