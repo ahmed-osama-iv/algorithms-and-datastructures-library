e55a235f9961d7254b02c1fbc1097fe3231ca911
ahmed-osama-iv <31954235+ahmed-osama-iv@users.noreply.github.com>
2019-10-22 15:41:51 +0200

    Update Combination.cpp

--- Math/Combinatorics/Combination.cpp
+++ Math/Combinatorics/Combination.cpp
@@ -1,13 +1,11 @@
-
 ll C[MAX][MAX];
 
-void build_nCr(){  // O(n^2)
-
+// O(n^2)
+void build_nCr() {
     C[0][0] = 1;
-    
-    for(int i = 1; i < MAX; i++){
+    for(int i = 1; i < MAX; i++) {
         C[i][0] = 1;
-        for(int j = 1; j <= i; j++){
+        for(int j = 1; j <= i; j++) {
             C[i][j] = C[i-1][j] + C[i-1][j-1];
             C[i][j] %= MOD;
         }
