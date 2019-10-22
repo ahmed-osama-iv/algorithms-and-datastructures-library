2c9364deb00d71e27aa478626921e93d51604bde
ahmed-osama-iv <31954235+ahmed-osama-iv@users.noreply.github.com>
2019-10-22 15:45:19 +0200

    Update Catalan_Number.cpp

--- Math/Combinatorics/Catalan_Number.cpp
+++ Math/Combinatorics/Catalan_Number.cpp
@@ -1,29 +1,24 @@
-
-ll Cat[MAX];
-
 // [https://en.wikipedia.org/wiki/Catalan_number]
 //Catalan(n) =~ 4^n / ( n^(3/2) * sqrt(PI) )    grows exponential
 
-void build_catalan() {  // O(n^2) "doesn't need mod inverse"
-    
-    Cat[0]=1;
+ll Cat[MAX];
 
-    for(int i = 0; i < MAX - 1; i++){
+// O(n^2) "doesn't need mod inverse"
+void build_catalan() {
+    Cat[0] = 1;
+    for(int i = 0; i < MAX - 1; i++) {
         Cat[i+1]=0;
-
-        for(int j = 0; j <= i; j++){
+        for(int j = 0; j <= i; j++) {
             Cat[i+1] += Cat[j] * Cat[i-j] % MOD;
             Cat[i+1] %= MOD;
         }
     }
 }
 
-
-void build_catalan(){  // O(n)
-
+// O(n)
+void build_catalan() {
     Cat[0] = 1;
-
-    for(int i = 0; i < MAX - 1; i++){
+    for(int i = 0; i < MAX - 1; i++) {
         Cat[i+1] = 2*(2*i+1) * Cat[i] % MOD * fast_pow(i+2, MOD-2, MOD) % MOD;
     }
 }
